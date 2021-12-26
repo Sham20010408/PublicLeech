25567baaea6fa65e941081b5e6ad33922c46c69a
Sham20010408 <90213871+Sham20010408@users.noreply.github.com>
2021-12-26 03:11:02 -0800

    Update and rename sample_config.py to config.py

---
+++ publicleechgroup/config.py
@@ -0,0 +1,59 @@
+    TG_BOT_TOKEN = 2132684011:AAGOge2QHLj7yFm8WaA9iyXuPBFC99K9oW4
+    # The Telegram API things
+    APP_ID = 1424314
+    API_HASH = d1c1c9262bbae8f5eeb80ba47c9f3dff
+    # Get these values from my.telegram.org
+    # array to store the channel ID who are authorized to use the bot
+    AUTH_CHANNEL = -1001410465876
+    # the download location, where the HTTP Server runs
+    DOWNLOAD_LOCATION = ./DOWNLOADS
+    # Telegram maximum file upload size
+    MAX_FILE_SIZE = 2097152000
+    TG_MAX_FILE_SIZE = 2097152000
+    FREE_USER_MAX_FILE_SIZE = 2097152000
+    )
+    # chunk size that should be used with requests
+    CHUNK_SIZE = 128
+    # default thumbnail to be used in the videos
+    DEF_THUMB_NAIL_VID_S = https://telegra.ph/file/8b973b270f4f380a427b1.png
+    )
+    # maximum message length in Telegram
+    MAX_MESSAGE_LENGTH = 4096
+    # set timeout for subprocess
+    PROCESS_MAX_TIMEOUT = 3600
+    #
+    ARIA_TWO_STARTED_PORT = 6800
+    EDIT_SLEEP_TIME_OUT = 2
+    MAX_TIME_TO_WAIT_FOR_TORRENTS_TO_START = 600
+    MAX_TG_SPLIT_FILE_SIZE = 2097152000
+    # add config vars for the display progress
+    FINISHED_PROGRESS_STR = █
+    UN_FINISHED_PROGRESS_STR = ░
+    # add offensive API
+    TG_OFFENSIVE_API = None
+    # URL for the rclone configuration
+    R_CLONE_CONF_URI = None
+    # Destination folder for the rclone
+    R_CLONE_DEST = /PublicLeech
+    # because, https://t.me/c/1494623325/5603
+    SHOULD_USE_BUTTONS = False
+    #
+    LOG_FILE_ZZGEVC = get_config("LOG_FILE_ZZGEVC", "PublicLeech.log")
+    #
+    SP_LIT_ALGO_RITH_M = get_config(
+        "SP_LIT_ALGO_RITH_M",
+        "hjs"
+    )
+    #
+    DIS_ABLE_ST_GFC_COMMAND_I = get_config(
+        "DIS_ABLE_ST_GFC_COMMAND_I",
+        False
+    )
+    #
+    SLEEP_THRES_HOLD = get_config(
+        "SLEEP_THRES_HOLD",
+        98712
+    )
+    # array to store the users who will have control (permissions)
+    # in the bot
+    SUDO_USERS = 1079261681
